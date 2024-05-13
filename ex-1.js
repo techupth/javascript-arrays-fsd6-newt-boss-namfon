18c9cb5997de9ca47ecd9dff646594fd6329ab62
Thanasak <ma.thanasak@gmail.com>
2024-05-13 14:34:02 +0700

    feature/newt rev.02

--- ex-1.js
+++ ex-1.js
@@ -4,7 +4,7 @@ let colors = ["red", "green", "blue", "khaki", "orange"];
 let blueColor = colors[2];
 let fourthColor = colors[3];
 let fifthColor = colors[4];
-let tenthColor = colors[10];
+let tenthColor = colors[9];
 console.log(blueColor);
 console.log(fourthColor);
 console.log(fifthColor);
