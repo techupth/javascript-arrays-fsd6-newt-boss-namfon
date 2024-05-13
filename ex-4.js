b24d09901bce9985f907985cbcbea1b2211b5ef6
Thanasak <ma.thanasak@gmail.com>
2024-05-13 13:25:46 +0700

    feat: Do arrays exercise 1-7

--- ex-4.js
+++ ex-4.js
@@ -12,3 +12,19 @@ const employees = [
 ];
 
 // Start coding here
+
+let alicia = {
+  name: "Alicia",
+  age: 29,
+  hobbies: ["Shopping", "Reading novels"],
+};
+
+let kody = {
+  name: "Kody",
+  age: 19,
+  hobbies: ["Computer games", "Wakeboard"],
+};
+
+employees.push(alicia);
+employees.push(kody);
+console.log(employees);
